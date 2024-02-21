3775afaaffd2e212c9482d048fa3afd075f5ee1b
Emilie <emma0003@stud.kea.dk>
2024-02-21 13:04:17 +0100

    hej

--- js/svamp.js
+++ js/svamp.js
@@ -3,13 +3,13 @@ console.log("siden er loaded");
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-const url = `https://dnrbmiiducergeuxsuzz.supabase.co/rest/v1/vildmad`;
+const url = `https://dnrbmiiducergeuxsuzz.supabase.co/rest/v1/vildmad/` + id;
 console.log("lort");
-
 fetch(url, {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucmJtaWlkdWNlcmdldXhzdXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDg5MzcsImV4cCI6MjAyMjg4NDkzN30.JK7N38zHew_AANCZ0wgtL_9sHiyvpaJr-i2mQO-9rjc",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucmJtaWlkdWNlcmdldXhzdXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDg5MzcsImV4cCI6MjAyMjg4NDkzN30.JK7N38zHew_AANCZ0wgtL_9sHiyvpaJr-i2mQO-9rjc",
   },
 })
   .then((response) => response.json())
@@ -22,7 +22,7 @@ function showProducts(mushroom) {
 function showProduct(mushroom) {
   console.log(mushroom);
 
-  document.querySelector("h1").textContent = mushroom.title;
-  document.querySelector(".beskrivelse p").textContent = mushroom.decription;
-  document.querySelector(".grid1 img").src = mushroom.images;
+  document.querySelector("h1").textContent = mushroom[0].title;
+  document.querySelector(".beskrivelse p").textContent = mushroom[0].decription;
+  document.querySelector(".grid1 img").src = mushroom[0].images;
 }
