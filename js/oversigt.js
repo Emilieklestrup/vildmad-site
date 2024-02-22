bf4028013de3675f4400694c1af60efda55bf598
sara gronkjaer <sagr0001@stud.kea.dk>
2024-02-22 08:53:43 +0100

    JavaScript

--- js/oversigt.js
+++ js/oversigt.js
@@ -1,21 +1,28 @@
 console.log("siden er loaded");
 
-// const urlParams = new URLSearchParams(window.location.search);
-// const id = urlParams.get("id");
+const urlParams = new URLSearchParams(window.location.search);
+const season = urlParams.get("season");
 
 const url = `https://dnrbmiiducergeuxsuzz.supabase.co/rest/v1/vildmad`;
 
 fetch(url, {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucmJtaWlkdWNlcmdldXhzdXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDg5MzcsImV4cCI6MjAyMjg4NDkzN30.JK7N38zHew_AANCZ0wgtL_9sHiyvpaJr-i2mQO-9rjc",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucmJtaWlkdWNlcmdldXhzdXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDg5MzcsImV4cCI6MjAyMjg4NDkzN30.JK7N38zHew_AANCZ0wgtL_9sHiyvpaJr-i2mQO-9rjc",
   },
 })
   .then((response) => response.json())
   .then((data) => showProducts(data));
-function showProducts(mushroom) {
-  console.log("her kommer mine mushroom: ", mushroom);
-  mushroom.forEach(showProduct);
+function showProducts(mushrooms) {
+  console.log("her kommer mine mushroom: ", mushrooms);
+  if (season) {
+    mushrooms = mushrooms
+      .sort(() => (Math.random() > 0.5 ? 1 : -1))
+      .slice(0, Math.random() * 3 + 4);
+  }
+
+  mushrooms.forEach(showProduct);
 }
 
 function showProduct(mushroom, index) {
@@ -26,7 +33,9 @@ function showProduct(mushroom, index) {
   copy.querySelector(".smallProduct h2").textContent = mushroom.title;
   copy.querySelector("img").src = mushroom.images;
 
-  copy.querySelector(".svamp_link").setAttribute("href", `svamp.html?id=${mushroom.id}`);
+  copy
+    .querySelector(".svamp_link")
+    .setAttribute("href", `svamp.html?id=${mushroom.id}`);
 
   document.querySelector(".grid_3").appendChild(copy);
 }
--- js/svamp.js
+++ js/svamp.js
@@ -8,14 +8,16 @@ console.log("lort");
 fetch(url, {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucmJtaWlkdWNlcmdldXhzdXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDg5MzcsImV4cCI6MjAyMjg4NDkzN30.JK7N38zHew_AANCZ0wgtL_9sHiyvpaJr-i2mQO-9rjc",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucmJtaWlkdWNlcmdldXhzdXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczMDg5MzcsImV4cCI6MjAyMjg4NDkzN30.JK7N38zHew_AANCZ0wgtL_9sHiyvpaJr-i2mQO-9rjc",
   },
 })
   .then((response) => response.json())
   .then((data) => showProduct(data));
 
-function showProduct(mushroom) {
-  console.log(mushroom);
+function showProduct(data) {
+  console.log(data);
+  mushroom = data[0];
   document.querySelector("h1").textContent = mushroom.title;
   document.querySelector("p").textContent = mushroom.description;
   document.querySelector(".grid1 img").src = mushroom.images;
